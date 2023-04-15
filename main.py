d45f253acffe16e64046f1260668f0c2dae37d33
kirill <kirill@gmail.com>
2023-04-16 01:29:53 +0300

    almost done with graphics

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import random
+from copy import deepcopy
 import json
+import pygame
 import time
+import color, stddraw
+from picture import Picture
 
-JAIL_POSITION = 11
-FREE_PARKING_POSITION = 21
-GO_TO_JAIL_POSITION = 31
-RAILROADS_POSITIONS= [6, 16, 26, 36]
-UTILITIES_POSITIONS = [13, 29]
+JAIL_POSITION = 10
+FREE_PARKING_POSITION = 20
+GO_TO_JAIL_POSITION = 30
+RAILROADS_POSITIONS= [5, 15, 25, 35]
+UTILITIES_POSITIONS = [12, 28]
 
 class Card:
-	def __init__(self, position, name, id): 
-		self.position = position
-		self.name = name	
+	def __init__(self, position, name, id):
+		self.position = position - 1
+		self.name = name
 		self.id = id
 
 class Property(Card):
-	def __init__(self, position, name, id, price): 
+	def __init__(self, position, name, id, price):
 		super().__init__(position, name, id)
 		self.price = price
 		self.owner = None
@@ -27,7 +31,7 @@ class Property(Card):
 class Street(Property):
 	def __init__(self, position, name, id, price, rent, group, housecost):
 		super().__init__(position, name, id, price)
-		self.buildings = 0
+		self.buildings = 8
 		self.rent = rent
 		self.group = group
 		self.househost = housecost
@@ -70,54 +74,63 @@ class Player:
 		self.money -= property.price
 		property.set_owner(self)
 		print(f"{self.name} bought {property.name} for {property.price} and has {self.money}$ left")
+		return f"{self.name} bought {property.name} for {property.price} and has {self.money}$ left"
 
 	def pay_other_player(self, other_player, amount):
-		self.money -= amount 
+		self.money -= amount
 		other_player.money += amount
 		print(f"{self.name} paid {other_player.name} {amount}$ and now has {self.money}$ left")
+		return f"{self.name} paid {other_player.name} {amount}$ and now has {self.money}$ left"
 
 	def go_to_card(self, card):
-		print(card)
 		self.position = card.position
-		self.action(card)
+		return self.action(card)
 
 	def throw_dice(self):
 		if random.randint(1, 6) == random.randint(1, 6):
 			return True
 		return False
-	
+
 	def cross_start(self):
 		self.money += 200
 		print(f'{self.name} started a new lap and gets 200$ from the Bank')
+		return f'{self.name} started a new lap and gets 200$ from the Bank'
 
 	def pay_tax(self, amount):
 		self.money -= 200
 		print(f'{self.name} paid {amount}$ in taxes')
+		return f'{self.name} paid {amount}$ in taxes'
 
 
 	def leave_jail(self):
 		if self.jail_free:
 			self.jail_free = False
-			self.in_jail = False 
+			self.in_jail = False
 			print(f'{self.name} uses Get out of jail card')
+			return f'{self.name} uses Get out of jail card'
 		elif self.money >= 50:
 			self.money -= 50
 			self.in_jail = False
 			print(f'{self.name} pays fine and gets out of jail')
+			return f'{self.name} pays fine and gets out of jail'
 		else:
 			if self.throw_dice():
 				self.in_jail = False
 				print(f'{self.name} gets lucky and gets out of jail')
+				return f'{self.name} gets lucky and gets out of jail'
+		print(f"{self.name} couldn't get out of jail")
+		return f"{self.name} couldn't get out of jail"
 
 	def action(self, card, dice_value=None):
 		card_type = type(card).__name__
 		if type(card).__bases__[0].__name__ == 'Property':
 			if card.owner is None:
 				if self.money >= card.price:
-					self.buy_property(card)
+					return self.buy_property(card)
 				else:
 					print(f"{self.name} can't afford {card.name}")
-			else:
+					return f"{self.name} can't afford {card.name}"
+			elif card.owner is not self:
 				if type(card).__name__ == 'Utility':
 					if len(card.owner.utilities) == 1:
 						rent = self.game.dice_value * 4
@@ -127,35 +140,46 @@ class Player:
 					rent = len(card.owner.railroads) * 25
 				else:
 					rent = card.rent
-				self.pay_other_player(card.owner, rent) 
-		
+				return self.pay_other_player(card.owner, rent)
+			else:
+				print(f"{self.name} has landed on his own card: {card.name}")
+				return f"{self.name} has landed on his own card: {card.name}"
+				
+
 		elif card_type == 'Special':
 			id = card.id
 			if id == 'incometax':
-				self.pay_tax(200)
+				return self.pay_tax(200)
 			elif id == 'jail':
 				if self.in_jail:
-					self.leave_jail()
+					return self.leave_jail()
 				else:
 					print(f'{self.name} is just visiting the jail')
+					return f'{self.name} is just visiting the jail'
 			elif id == 'chance' or id == 'communitychest':
 				effect = random.choice(game.chance_cards + game.chests)
-				self.get_effect(effect)
+				return self.get_effect(effect)
 			elif id == 'freeparking':
 				print(f'{self.name} is resting in a free parking')
+				return f'{self.name} is resting in a free parking'
 			elif id == 'gotojail':
-				self.position == 11
+				self.position = JAIL_POSITION
+				print(self.position, JAIL_POSITION)
 				self.in_jail = True
+				print(f'{self.name} commits a crime and goes to jail')
+				return f'{self.name} commits a crime and goes to jail'
 			elif id == 'luxurytax':
-				self.pay_tax(100)
-				
+				return self.pay_tax(100)
+			elif id == 'go':
+				print(f"{self.name} has just landed on the go square")
+				return f"{self.name} has just landed on the go square"
+
 	def get_effect(self, card):
-		print(self.name + ': ' + card.title)
 		if card.action == 'move':
 			id = card.tileid
 			new_card = self.game.get_card_by_id(id)
-			self.go_to_card(new_card)
-		
+			return self.go_to_card(new_card)
+
 		elif card.action == 'addfunds':
 			self.money += card.amount
 		elif card.action == 'addfundsfromplayers':
@@ -188,7 +212,9 @@ class Player:
 				if distance < nearest_distance:
 					nearest_distance = distance
 					nearest_card = self.game.cards[card_position]
-			self.go_to_card(nearest_card)
+			return self.go_to_card(nearest_card)
+		print(self.name + ': ' + card.title)
+		return self.name + ': ' + card.title
 
 
 
@@ -231,6 +257,8 @@ class Game:
 		self.players = []
 		self.is_finished = False
 		self.dice_value = None
+		print(pygame.font.get_fonts())
+		self.font = pygame.font.SysFont('Arial', 15)
 		with open('src/monopoly.json') as f:
 			game_data = json.loads(f.read())
 			properties = game_data['properties']
@@ -273,25 +301,128 @@ class Game:
 				hotels = card.get('hotels'))
 				self.chests.append(chest)
 
+		pygame.init()
+		self.screen_w = 1920
+		self.screen_h = 1080
+		self.screen = pygame.display.set_mode((1920, 1080), pygame.RESIZABLE)
+		self.screen.fill((255, 255, 255))
+		self.field_w = 950
+		self.field_h = 950
+		self.field_image= pygame.transform.scale(pygame.image.load('src/field.jpg'), (self.field_w, self.field_h))
+		self.dice_sides = ['src/dice1.png', 'src/dice2.png', 'src/dice3.png', 'src/dice4.png','src/dice5.png','src/dice6.png']
+		self.players_pictures = [pygame.transform.scale(pygame.image.load('src/battleship.png'), (80, 80)),
+		pygame.transform.scale(pygame.image.load('src/car.png'), (80, 80))]
+
 	def play(self):
 		self.players = [Player('Player 1', game), Player('Player 2', game)]
+
 		turn = 0
+		self.draw_players()
+		self.screen.blit(self.field_image, ((self.screen_w - self.field_w) // 2, 10))
+		pygame.display.flip()
+		time.sleep(5)
 		while not self.is_finished:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.is_finished = True
+			self.screen.fill((255, 255, 255))
+			dice1, dice2 = (random.randint(1, 6), random.randint(1, 6))
+			self.dice_value = dice1 + dice2
+
 			current_player = self.players[turn]
-			self.dice_value = random.randint(1, 12)
-			new_position = (current_player.position + self.dice_value - 1) % 40  
-			print(new_position)
-			current_card = self.cards[new_position] 
-			current_player.go_to_card(current_card)
+			if current_player.in_jail:
+				new_position = current_player.position
+			else:
+				new_position = (current_player.position + self.dice_value) % 40
+			current_card = self.cards[new_position]
+			message = current_player.go_to_card(current_card)
 			turn  = (turn + 1) % 2
 			if current_player.money < 0:
-				self.declare_winner(self.players[turn])
+				message = self.declare_winner(self.players[turn])
+			self.draw_dices(dice1, dice2)
+			self.screen.blit(self.field_image, ((self.screen_w - self.field_w) // 2, 10))
+			pygame.display.update()
+			#time.sleep(2)
+			input()
+			
+			self.field_image= pygame.transform.scale(pygame.image.load('src/field.jpg'), (self.field_w, self.field_h))
+			self.draw_dices(dice1, dice2)
+			self.draw_players()
+			self.show_stats()
+			self.show_message(message)
+			print(message + 'lol')
+			self.screen.blit(self.field_image, ((self.screen_w - self.field_w) // 2, 10))
+			
+			pygame.display.flip()
+			#time.sleep(5)
+			input()
+
 
 	def declare_winner(self, player):
 		self.is_finished = True
 		print(f'Game over. {player.name} is victorious!')
+		return f'Game over. {player.name} is victorious!'
 
+
+	def draw_dices(self, dice1, dice2):
+		dice_file1, dice_file2 = self.dice_sides[dice1 - 1], self.dice_sides[dice2 - 1]
+		dice_image1, dice_image2 = (pygame.transform.scale(pygame.image.load(dice_file1), (50, 50)),
+				pygame.transform.scale(pygame.image.load(dice_file2), (50, 50)))
+		self.field_image.blit(dice_image1, (460, 700))
+		self.field_image.blit(dice_image2, (560, 700))
+
+
+	def show_message(self, message):
+		self.message_box = pygame.Surface((500, 30))	
+		self.message_box.fill((220, 220, 220, 0.3))
+		self.message_box.blit(self.font.render((message), True, (255, 0, 0)), (5, 5))
+		self.field_image.blit(self.message_box, (120, 600))
 	
+
+	def show_stats(self):
+		player1, player2 = self.players[0], self.players[1]
+		stats_player1 = pygame.Surface((400, 1000))
+		stats_player2 = pygame.Surface((400, 1000))
+
+		stats_player1.fill((255, 255, 255))
+		stats_player2.fill((255, 255, 255))
+
+		stats_player1.blit(self.font.render((player1.name), True, (0, 0, 0)), (5,5))
+		stats_player2.blit(self.font.render((player2.name), True, (0, 0, 0)), (5,5))
+
+		self.screen.blit(stats_player1, (5, 5))
+		self.screen.blit(stats_player2, (1450, 5))
+
+
+	def draw_players(self):
+		tile_w = tile_h = self.field_w * 0.08
+		big_tile_w = self.field_h * 0.13
+		for i in range(2):
+			
+			player = self.players[i]
+			position = player.position 
+			print(position)
+			if position // 10 == 0:
+				coords_x = self.field_w - big_tile_w  - tile_w * (position  % 10)
+				coords_y = 800
+			elif position // 10 == 1:
+				coords_x = 50
+				coords_y = self.field_h - big_tile_w - tile_h * (position   % 10)
+			elif position // 10 == 2:
+				coords_x = big_tile_w + tile_w * (position   % 10) - 80
+				coords_y = 50
+			elif position // 10 == 3:
+				coords_x = self.field_w - 120
+				coords_y = big_tile_w + tile_h * (position % 10) - 80
+			self.field_image.blit(self.players_pictures[i], (coords_x, coords_y))
+
+
+
+
+
+
+
+
 	def get_card_by_id(self, id):
 		for card in self.cards:
 			if card.id == id:
